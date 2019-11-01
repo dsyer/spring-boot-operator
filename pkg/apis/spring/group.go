5df716a8f75bdfe65349fff24a0cf36e35b21696
Dave Syer <dsyer@pivotal.io>
2019-11-01 08:36:28 +0000

    Alternative version with kubebuilder

--- pkg/apis/spring/group.go
+++
@@ -1,6 +0,0 @@
-// Package spring contains spring API versions.
-//
-// This file ensures Go source parsers acknowledge the spring package
-// and any child packages. It can be removed if any other Go source files are
-// added to this package.
-package spring
--- pkg/apis/spring/v1alpha1/doc.go
+++
@@ -1,4 +0,0 @@
-// Package v1alpha1 contains API Schema definitions for the spring v1alpha1 API group
-// +k8s:deepcopy-gen=package,register
-// +groupName=spring.io
-package v1alpha1

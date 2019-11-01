e8673a4805d0926c9be83a8bf41ff797674d2faf
Dave Syer <dsyer@pivotal.io>
2019-11-01 09:29:39 +0000

    After adding property to CRD

--- api/v1/spring_types.go
+++ api/v1/spring_types.go
@@ -19,19 +19,13 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // SpringSpec defines the desired state of Spring
 type SpringSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	Foo string `json:"foo,omitempty"`
 }
 
 // SpringStatus defines the observed state of Spring
 type SpringStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
 }
 
 // +kubebuilder:object:root=true
